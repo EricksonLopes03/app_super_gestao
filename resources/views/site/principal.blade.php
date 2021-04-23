567b84bc59deccf93841aba66e26767f8bd05e7f
EricksonLopes03 <ericksonl.santos@hotmail.com>
2021-04-23 13:32:59 -0300

    Personalizando rotas e tratamento basico da rota de fallback

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,1 +1,7 @@
 <h1>Principal</h1>
+
+<ul>
+    <li><a href="{{ route('site.principal') }}"> Principal</a></li>
+    <li><a href="{{ route('site.sobre-nos') }}"> Sobre Nos</a></li>
+    <li><a href="{{ route('site.contato') }}"> Contato</a> </li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.principal');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+
+Route::get('/login', function(){ return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'produtos';})->name('app.produtos');
+});
+
+Route::fallback(function(){
+    return 'A página acessada não existe. <a href="'.route('site.principal').'"> Clique aqui </a> para voltar para a página principal';
+});
